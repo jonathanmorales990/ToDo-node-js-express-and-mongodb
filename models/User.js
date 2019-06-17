f5d34e3e5ffde88d5eddd67290382fe611d42f27
Jonathan Morales <johnmorales990@gmail.com>
2019-06-17 18:05:27 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,20 +4,20 @@ const Todo = require('./Todo');
 
 const userSchema = mongoose.Schema({
 	nome: String,
-    sobrenome: String,
-    email: String,
-    password: String,
-    todos: [Todo]
+	sobrenome: String,
+	email: String,
+	password: String,
+	todos: [Todo]
 },{
-    collection: 'usuarios'
+    	collection: 'usuarios'
 });
 // generating a hash
 userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 // checking if password is valid
 userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
+    	return bcrypt.compareSync(password, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);

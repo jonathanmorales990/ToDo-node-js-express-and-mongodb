06eca3386d96d0f7ffe24b747f730070c1d30ba0
Jonathan Morales <johnmorales990@gmail.com>
2019-06-17 18:01:12 -0300

    Update loginSignup.js

--- public/javascripts/loginSignup.js
+++ public/javascripts/loginSignup.js
@@ -1,8 +1,8 @@
 $(function() {
 
-    $('#login-form-link').click(function(e) {
+	$('#login-form-link').click(function(e) {
 		$("#login-form").delay(100).fadeIn(100);
- 		$("#register-form").fadeOut(100);
+		$("#register-form").fadeOut(100);
 		$('#register-form-link').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
@@ -10,7 +10,7 @@ $(function() {
 
 	$('#register-form-link').click(function(e) {
 		$("#register-form").delay(100).fadeIn(100);
- 		$("#login-form").fadeOut(100);
+		$("#login-form").fadeOut(100);
 		$('#login-form-link').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();

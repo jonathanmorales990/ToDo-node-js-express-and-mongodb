fe86f1bbec640f50237dba237bff0b07ee78c035
Jonathan Morales <johnmorales990@gmail.com>
2019-06-19 23:34:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ mongoose.Promise = require('bluebird');
 const loginSignupRouter = require('./routes/loginSignup');
 const todoRouter = require('./routes/todo');
 
-var session = require("express-session")({
+let session = require("express-session")({
     secret: 'todo!@$%()$#)@',
     resave: true,
     saveUninitialized: true
@@ -20,15 +20,13 @@ var session = require("express-session")({
 const app = express();
 
 app.use(flash());
-
 app.use(session);
-
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./config/passport')(passport);
 require('./config/connection')(mongoose);
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -41,18 +39,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', loginSignupRouter);
 app.use('/todo', todoRouter);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
     next(createError(404));
 });
 
-// error handler
 app.use((err, req, res, next) => {
-    // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
     res.status(err.status || 500);
     res.render('error');
 });
